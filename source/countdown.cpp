9e6365e878ce853db0bec62129648a106902fca3
Mauro Marssola <mauro@wvtodoz.com.br>
2017-11-13 16:55:12 -0200

    Fix time

--- source/countdown.cpp
+++ source/countdown.cpp
@@ -7,9 +7,7 @@ Countdown::Countdown(QObject *parent) : QObject(parent)
 
 void Countdown::setTime(QString get_time)
 {
-    //QDateTime time = QDateTime::fromString(QString("1970-01-01T%1").arg(get_time), Qt::ISODate);
-    QDateTime time;
-    time = QDateTime::fromString(QString("1970-01-01T00:00:00"), Qt::ISODate);
+    QDateTime time = QDateTime::fromString(QString("1970-01-01 %1 -00").arg(get_time), Qt::ISODate);
     qDebug() << time.toUTC().toTime_t();
 
     m_time_string = get_time;
